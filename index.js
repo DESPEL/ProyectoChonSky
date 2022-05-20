3874a8df82f3c635ab3edd7eab553218ebb3586d
jaxelc92 <axel.cruz92@gmail.com>
2022-05-19 17:17:54 -0700

    cambios en front y clear

--- index.js
+++ index.js
@@ -1,92 +1,115 @@
-const calculator = document.querySelector('.fcgcalculator')
-const keys = document.querySelector('.calculator__keys')
-const display = document.querySelector('.calculator__display1')
-const displayVar = document.querySelector('.variable__display')
-const arrow_display = document.querySelector('.arrow__display')
-const displayGLC = document.querySelector('.calculator__display3')
-const pbr = document.createElement('br')
-const epsilon = document.querySelector('.bepsilon')
-var var_index = 0
-var glcVars = []
-var glcTerms = []
-const vars =  ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","T","U","V","W","X","Y","Z"]
+const calculator = document.querySelector(".fcgcalculator");
+const keys = document.querySelector(".calculator__keys");
+const display = document.querySelector(".calculator__display1");
+const displayVar = document.querySelector(".variable__display");
+const arrow_display = document.querySelector(".arrow__display");
+const displayGLC = document.querySelector(".calculator__display3");
+const pbr = document.createElement("br");
+const epsilon = document.querySelector(".bepsilon");
+var var_index = 0;
+var glcVars = [];
+var glcTerms = [];
+const vars = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-
-
-keys.addEventListener('click', e => {
-  if (e.target.matches('button')) {
+keys.addEventListener("click", (e) => {
+  if (e.target.matches("button")) {
     //action
-    const key = e.target
-    const action = key.dataset.action
-    const keyContent = key.textContent
-    const displayedEpsilon = epsilon.textContent
-    const displayedVar = displayVar.textContent
-    const displayedChar = display.textContent
-    const displayedArrow = arrow_display.textContent
-    var displayedGLC = displayGLC.textContent
-    const previousKeyType = calculator.dataset.previousKeyType
- 
-    var usedVars = []
-    
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedEpsilon = epsilon.textContent;
+    const displayedVar = displayVar.textContent;
+    const displayedChar = display.textContent;
+    const displayedArrow = arrow_display.textContent;
+    var displayedGLC = displayGLC.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
+
+    var usedVars = [];
+
     if (!action) {
-      if (displayedChar === '') {
-        display.textContent = keyContent
-      } 
-      else {
-        display.textContent = displayedChar + keyContent
+      if (displayedChar === "") {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedChar + keyContent;
       }
     }
-    
-    if ( action == 'add_rule') {
-      if (displayedGLC === '') {
-        displayGLC.textContent = displayedVar + displayedArrow + displayedChar
-        displayedGLC = displayGLC.textContent
-        glcVars.push(displayedVar)
-        if(!displayedGLC.includes('|')) {
-          glcTerms.push(displayedChar)
+
+    if (action == "add_rule") {
+      if (displayedGLC === "") {
+        displayGLC.textContent = displayedVar + displayedArrow + displayedChar;
+        displayedGLC = displayGLC.textContent;
+        glcVars.push(displayedVar);
+        if (!displayedGLC.includes("|")) {
+          glcTerms.push(displayedChar);
         } else {
-          let or_count = displayedChar.split('|').length-1
-          displayedChar.replace('|', ' ')
-          glcTerms = displayedChar.split('|', or_count+1)
-          for (let i=0; i<or_count; i++) {
-            glcVars.push(displayedVar)
+          let or_count = displayedChar.split("|").length - 1;
+          displayedChar.replace("|", " ");
+          glcTerms = displayedChar.split("|", or_count + 1);
+          for (let i = 0; i < or_count; i++) {
+            glcVars.push(displayedVar);
           }
         }
       } else {
-        
-        let temp_array = []
-        displayedGLC = displayGLC.textContent + '\r'
-        displayGLC.textContent = displayedGLC + displayedVar + displayedArrow + displayedChar
-        if(!displayedGLC.includes('|')) {
-          glcVars.push(displayedVar)
-          glcTerms.push(displayedChar)
+        let temp_array = [];
+        displayedGLC = displayGLC.textContent + "\r";
+        displayGLC.textContent =
+          displayedGLC + displayedVar + displayedArrow + displayedChar;
+        if (!displayedGLC.includes("|")) {
+          glcVars.push(displayedVar);
+          glcTerms.push(displayedChar);
         } else {
-          let or_count = displayedChar.split('|').length
-          displayedChar.replace('|', ' ')
-          temp_array = displayedChar.split('|', or_count+1)
-          
-          for (let i=0; i<or_count; i++) {
-            glcVars.push(displayedVar)
-            glcTerms.push(temp_array[i])
+          let or_count = displayedChar.split("|").length;
+          displayedChar.replace("|", " ");
+          temp_array = displayedChar.split("|", or_count + 1);
+
+          for (let i = 0; i < or_count; i++) {
+            glcVars.push(displayedVar);
+            glcTerms.push(temp_array[i]);
           }
         }
-        
       }
-      displayVar.textContent = vars[var_index]
-      display.textContent = ''
-      var_index+=1
-      console.log('vars', glcVars)
-      console.log('rules', glcTerms)
+      displayVar.textContent = vars[var_index];
+      display.textContent = "";
+      var_index += 1;
+      console.log("vars", glcVars);
+      console.log("rules", glcTerms);
     }
-    
-    if (action == 'clear') {
-      console.log('clear key')
+
+    if (action == "clear") {
+      location.reload(true);
+      console.log("clear");
     }
-    
-    if (action == 'calculate') {
-      console.log("Calculate ? idk")
-      let glc = normalizeGLC(glcVars, glcTerms, "S")
-      console.log(glc.chomsky())
+
+    if (action == "calculate") {
+      console.log("Calculate ? idk");
+      let glc = normalizeGLC(glcVars, glcTerms, "S");
+      console.log(glc.chomsky());
     }
   }
-})
+});
